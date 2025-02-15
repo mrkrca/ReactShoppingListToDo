ebac77f63299e7004ad29dc96e4a3e5155b30013
mrkrca <aleksa.djuric.dj@gmail.com>
2025-02-15 16:43:44 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     port: 4173,
     host: true
+  },
+  preview: {
+    port: 4173,
+    allowedHosts: ['reactshoppinglisttodo-production.up.railway.app']
   },
   build: {
     outDir: 'dist'
